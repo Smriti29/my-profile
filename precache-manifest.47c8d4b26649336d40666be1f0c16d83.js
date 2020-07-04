c3b73526ee4144c7892a7287adf3ec894022b93e
smriti29 <smritip29@gmail.com>
2020-07-05 01:31:24 +0530

    Updates

---
+++ precache-manifest.47c8d4b26649336d40666be1f0c16d83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0fd0c431aaa6e7b65ba00daa6e9b987",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "b7f169c7d30567ca49f7",
+    "url": "/my-profile/static/css/main.7620cdf6.chunk.css"
+  },
+  {
+    "revision": "8bb01b64b02314828754",
+    "url": "/my-profile/static/js/2.fac51bf3.chunk.js"
+  },
+  {
+    "revision": "3fba9772045d46a99012b30017622963",
+    "url": "/my-profile/static/js/2.fac51bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f169c7d30567ca49f7",
+    "url": "/my-profile/static/js/main.0548cc68.chunk.js"
+  },
+  {
+    "revision": "6d8f0160b3eb678d55ad",
+    "url": "/my-profile/static/js/runtime-main.07a20771.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
